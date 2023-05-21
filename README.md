e87fe90aeb389a679333f4d96dec66b3e195cca5
eferreirafilho <edsonbffilho@gmail.com>
2023-05-21 16:43:17 +0100

    Added controllers

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-## Multi Agent Gazebo Simulation
+# Multi Agent Gazebo Simulation
 
-## To run
+Robot and world models heavily based on: https://bitbucket.org/theconstructcore/box_bot/src/foxy/
+
+## To launch gazebo world and N robots
 
 source install/setup.bash
- 
-ros2 launch box_bot_gazebo multi_box_bot_launch.py 
 
-# Observations
+ros2 launch box_bot_gazebo multi_box_bot_launch.py
+
+## Observations
 
 - Robots are differential drive
-- Number of robots is hardcoded
+- Number of robots is hardcoded for now
--- planning/scripts/planner.py
+++ planning/scripts/planner.py
@@ -2,18 +2,37 @@
 import rclpy
 from rclpy.node import Node
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Point
 import re
+import math
+from tf_transformations import euler_from_quaternion
+import random
+import time
+
+THRE_ROBOT_ON_TARGET = 0.1 # Thredshould to consider that robot has reached a target
+TARGETS_RANDOM_POOL_SIZE = 2 # Size of target area
+KP = 0.05 # Controller proportional gain
+MAX_LINEAR_VELOCITY = 0.8
+MAX_ANGULAR_VELOCITY = 0.2
 
 class Planner(Node):
     def __init__(self):
-        super().__init__('robot_position_reader')
-
+        super().__init__('robot_controller')
+        
         #TODO: automate number of robots
         self.robots = ['robot0', 'robot1', 'robot2']
         self.subscribers = []
-        self.positions = [[] for _ in range(len(self.robots))]
-
+        self.robot_publishers = []
+        self.positions = [Point() for _ in range(len(self.robots))]
+        self.orientations = [float for _ in range(len(self.robots))]
+        self.distance_to_target = [float for _ in range(len(self.robots))]
+        self.target_positions = [Point() for _ in range(len(self.robots))]
+        for robot_index, robot_name in enumerate(self.robots):
+            self.target_positions[int(robot_index)] = Point(x=float(random.randint(-TARGETS_RANDOM_POOL_SIZE, TARGETS_RANDOM_POOL_SIZE)), y=float(random.randint(-TARGETS_RANDOM_POOL_SIZE, TARGETS_RANDOM_POOL_SIZE)), z=0.01) # Random target positions
+        self.create_all_publishers()
+        self.create_all_subscribers()
+    
+    def create_all_publishers(self):
         for robot_name in self.robots:
             topic = f'/{robot_name}/odom'
             subscriber = self.create_subscription(
@@ -24,27 +43,89 @@ class Planner(Node):
             )
             subscriber.robot_name = robot_name
             self.subscribers.append(subscriber)
+    
+    def create_all_subscribers(self):
+        for robot_name in self.robots:
+            topic = f'/{robot_name}/cmd_vel'
+            publisher = self.create_publisher(
+                Twist,
+                topic,
+                10
+            )
+            self.robot_publishers.append(publisher)
 
     def position_callback(self, msg, topic):
         robot_index = re.search(r'/robot(\d+)/odom', topic).group(1)
         position = msg.pose.pose.position
-        # self.get_logger().info(f'{robot_index} position: x={position.x}, y={position.y}, z={position.z}')
-        self.positions[int(robot_index)].append([position.x, position.y, position.z])
+        orientation = msg.pose.pose.orientation
+        (roll, pitch, theta)  = euler_from_quaternion([orientation.x, orientation.y, orientation.z, orientation.w])
+        self.positions[int(robot_index)] = Point(x=position.x, y=position.y, z=position.z)
+        self.orientations[int(robot_index)] = theta
         
-    def test_pub(self):
-        publisher = self.create_publisher(Twist, '/robot0/cmd_vel', 10)
-        pub_msg = Twist()
-        # pub_msg.linear.x = 0.05
-        pub_msg.angular.z = 0.05
-        publisher.publish(pub_msg)
-        print(pub_msg)
+        # Compute a new control input for every update in position
+        self.drive_robots_to_random_positions()
+        
+    def drive_robots_to_random_positions(self):
+        while not self.positions or not self.orientations:
+            self.get_logger().info('Waiting for current position...')
+            rclpy.spin_once(self)
+        
+        # Check if all robots arrived in target positions
+        number_of_robots_in_target = 0
+        for robot_index, _ in enumerate(self.robots):
+            current_position = self.positions[robot_index]
+            current_orientation = self.orientations[robot_index]
+            self.distance_to_target[int(robot_index)] = self.get_distance_to_target(self.target_positions[int(robot_index)].x - current_position.x, self.target_positions[int(robot_index)].y - current_position.y)
+            if self.distance_to_target[int(robot_index)] < THRE_ROBOT_ON_TARGET:
+                number_of_robots_in_target += 1 
+        
+        if int(number_of_robots_in_target) == int(len(self.robots)): # all robots arrived -> new targets
+            for robot_index, _ in enumerate(self.robots):
+                self.get_logger().info('All robots arrived on targets')
+                self.get_logger().info('Acquiring new target')
+                self.target_positions[int(robot_index)] = Point(x=float(random.randint(-TARGETS_RANDOM_POOL_SIZE, TARGETS_RANDOM_POOL_SIZE)), y=float(random.randint(-TARGETS_RANDOM_POOL_SIZE, TARGETS_RANDOM_POOL_SIZE)), z=0.01) # Random target positions
+                time.sleep(2)
+
+        rate = self.create_rate(1)
+        
+        d = 0.1 # Virtual point outside robot center (avoid mathematical errors in the feedback linearization controller)
+        for robot_index, robot_name in enumerate(self.robots):
+            current_position = self.positions[robot_index]
+            current_orientation = self.orientations[robot_index]
+            target_position = self.target_positions[robot_index]
+            cmd_vel = Twist()
+
+            # Validate current_orientation
+            if isinstance(current_orientation, (int, float)):
+                current_orientation = float(current_orientation)
+            else:
+                error_msg = "Invalid current_orientation: " + str(current_orientation)
+                self.get_logger().error(error_msg)
+                continue
+            try: # Feedback linearization controller
+                cmd_vel.linear.x = (target_position.x - current_position.x) * math.cos(current_orientation) + (target_position.y - current_position.y) * math.sin(current_orientation)
+                cmd_vel.angular.z = -(target_position.x - current_position.x) * math.sin(current_orientation) / d + (target_position.y - current_position.y) * math.cos(current_orientation) / d
+                cmd_vel.linear.x = KP * cmd_vel.linear.x
+                cmd_vel.angular.z = KP * cmd_vel.angular.z
+                # Limit the velocities
+                cmd_vel.linear.x = min(cmd_vel.linear.x, MAX_LINEAR_VELOCITY)
+                cmd_vel.angular.z = min(cmd_vel.angular.z, MAX_ANGULAR_VELOCITY)
+                self.robot_publishers[int(robot_index)].publish(cmd_vel)
+            except Exception as e:
+                error_msg = "Error occurred during computation: " + str(e)
+                self.get_logger().error(error_msg)
+            
+            # self.get_logger().info(f'Robot: {robot_index}, Target: {target_position}, Current: {current_position}')
+
+    def get_distance_to_target(self, dx, dy):
+        # Calculate the Euclidean distance between the current position and the target position
+        return math.sqrt(dx*dx + dy*dy)
 
 def main(args=None):
     rclpy.init(args=args)
-    position_reader = Planner()
-    position_reader.test_pub()
-    rclpy.spin(position_reader)
-    position_reader.destroy_node()
+    planning_and_control = Planner()
+    rclpy.spin(planning_and_control)
+    planning_and_control.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
