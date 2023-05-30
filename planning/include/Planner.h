6072913ad956ff2b9772e67ebc069c3545cfb41a
Swapnil Bhat <swapnilbhat.iitb@gmail.com>
2023-05-31 01:14:52 +0530

    feat: adding member variables to planne class

--- planning/include/Planner.h
+++ planning/include/Planner.h
@@ -29,6 +29,30 @@ class Planner : public rclcpp::Node {
   void init();
   void createAllSubscribers();
   void createAllPublishers();
+
+  bool custom_goals_ =
+      False;  // True: read positions from /params/custom_goals.yaml
+              // False: Random targets
+  bool replan_ =
+      True;  // True: Plan and execute continuosly False: Plan and execute once
+  float threshold_bot_on_target_ =
+      0.1;  // Threshold to consider that robot has reached a target
+  unsigned short targets_random_pool_size_ =
+      3;             // Size of target area in meters (Gazebo squares)
+  float Kp_ = 0.15;  // Controller "proportional" gain
+  unsigned short max_linear_velocity_ = 1;
+  float max_angular_velocity_ = 0.2;
+  unsigned short discretization_ =
+      2;  // Discretization of map(1: one point per gazebo square,
+          // 2 : 4 points per gazebo square)
+  unsigned short shift_map_ =
+      10 *
+      discretization_;  // CBS only accepts positive integer values, all the
+                        // values used by the CBS are shifted beforehand.10
+                        // guarantees that if robots are inside gazebo
+                        // dafault plane,all points sent to CBS are positive
+  unsigned short cbs_map_dimension_ =
+      20 * discretization_;  // This calculate the whole gazebo default area
 };
 }  // namespace TCAS
 #endif
