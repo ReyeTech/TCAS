1dc1655181ab81aee0655a9eec7ff5dc0a7504d0
Nazneen Khan <knazneen123@gmail.com>
2023-06-05 21:58:33 +0900

    feat: added turtlebot3 world

--- box_bot_gazebo/launch/start_world_launch.py
+++ box_bot_gazebo/launch/start_world_launch.py
@@ -27,7 +27,7 @@ def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument(
           'world',
-          default_value=[os.path.join(pkg_box_bot_gazebo, 'worlds', 'box_bot_empty.world'), ''],
+          default_value=[os.path.join(pkg_box_bot_gazebo, 'worlds', 'turtlebot3_world.world'), ''],
           description='SDF world file'),
         gazebo
     ])
